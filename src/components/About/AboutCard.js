0d7cdfc305d7528f3fdbab25a0a2abb790167af8
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-10-16 16:14:25 +0530

    Task-5 React

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am{" "}
+            <span className="purple">Shashwat Wankhedekar </span>
+            from <span className="purple"> Haryana, India.</span>
+            <br /> I am a Third Year student pursuing B.Tech. in CSE from VIT
+            Vellore.
+            <br />
+            Additionally, I am enthusiastic about Blockchain Development.
+            <br />
+            <br />
+            Some of my hobbies are
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Partying
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching Movies
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
